f86aa48414c558966e0b2b8e17c549eef236b1d4
ErrkkSkyseer <lolplayer78876@gmail.com>
2025-01-24 15:39:34 +0700

    Lab14 Completed!

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -2,18 +2,53 @@
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[],int N){
-
+void show(T arr[], int N)
+{
+	for (int i = 0; i < N; i++)
+		cout << arr[i] << " ";
+	cout << endl;
 }
 
-int main(){
-	int a[10] = {12,25,30,44,2,0,4,7,55,25};
+template <typename T>
+void doInsert(T arr[], int elem)
+{
+	T temp = arr[elem];
+	int stopIndex = 0;
+	for (int i = elem; i >= 1; i--)
+	{
+		if (temp > arr[i - 1])
+			arr[i] = arr[i - 1];
+		else
+		{
+			stopIndex = i;
+			break;
+		}
+		}
+	arr[stopIndex] = temp;
+}
+
+template <typename T>
+void insertionSort(T d[], int N)
+{
+	for (int i = 1; i < N; i++)
+	{
+		doInsert(d, i);
+		cout << "Pass " << i << ":";
+		show(d, N);
+	}
+}
+
+int main()
+{
+	int a[10] = {12, 25, 30, 44, 2, 0, 4, 7, 55, 25};
 	cout << "Input Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";
+	for (int i = 0; i < 10; i++)
+		cout << a[i] << " ";
 	cout << "\n\n";
-	
-	insertionSort(a,10);
-	
+
+	insertionSort(a, 10);
+
 	cout << "\nSorted Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+	for (int i = 0; i < 10; i++)
+		cout << a[i] << " ";
 }
--- lab14_2.cpp
+++ lab14_2.cpp
@@ -1,23 +1,67 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 const int N = 5;
 
-void inputMatrix(double [][N]);
+void inputMatrix(double[][N]);
 
-void findLocalMax(const double [][N], bool [][N]);
+void findLocalMax(const double[][N], bool[][N]);
 
-void showMatrix(const bool [][N]);
+void showMatrix(const bool[][N]);
 
-int main(){
-	double A[N][N]; 
-	bool B[N][N]; 
+int main()
+{
+	double A[N][N];
+	bool B[N][N];
 	cout << "Input the matrix.....\n";
 	inputMatrix(A);
-	findLocalMax(A,B);
+	findLocalMax(A, B);
 	cout << "Result is \n";
 	showMatrix(B);
 	return 0;
 }
 
-//Do not modify source code above this line 
+// Do not modify source code above this line
+void inputMatrix(double mtx[][N])
+{
+	for (int m = 0; m < N; m++)
+	{
+		cout << "Row " << m + 1 << ": ";
+		for (int n = 0; n < N; n++)
+		{
+			cin >> mtx[m][n];
+		}
+	}
+}
+
+void findLocalMax(const double mtx[][N], bool ans[][N])
+{
+	for (int m = 0; m < N; m++)
+	{
+		for (int n = 0; n < N; n++)
+		{
+			if (m == 0 || m == N - 1 || n == 0 || n == N - 1)
+			{
+				ans[m][n] = false;
+				continue;
+			}
+
+			if (mtx[m][n] >= mtx[m + 1][n] && mtx[m][n] >= mtx[m][n + 1] && mtx[m][n] >= mtx[m - 1][n] && mtx[m][n] >= mtx[m][n - 1])
+				ans[m][n] = true;
+			else
+				ans[m][n] = false;
+		}
+	}
+}
+
+void showMatrix(const bool mtx[][N])
+{
+	for (int m = 0; m < N; m++)
+	{
+		for (int n = 0; n < N; n++)
+		{
+			cout << mtx[m][n] << " ";
+		}
+		cout << "\n";
+	}
+}
